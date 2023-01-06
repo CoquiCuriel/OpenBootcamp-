2a3a002da3c4af981210b39312fbfc5ffb7afaa8
Ricardo Curiel <66967495+CoquiCuriel@users.noreply.github.com>
2023-01-05 23:54:20 -0300

    Add files via upload
    Creación de modulo con operaciones matemáticas básicas. El cual es importado y utilizado desde otro archivo.

---
+++ ejercicio modulos/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 23:49:14 2023
+
+@author: Coqui
+"""
+
+import operaciones
+
+suma = operaciones.suma(10, 15)
+print("La suma es:",suma)
+
+resta = operaciones.resta(15, 10)
+print("La resta es:",resta)
+
+divide = operaciones.divide(15, 3)
+print("La division es:",divide)
+
+multiplica = operaciones.multiplica(5, 3)
+print("La multiplicación es:",multiplica)
---
+++ ejercicio modulos/operaciones.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 21:14:22 2023
+
+@author: Coqui
+"""
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplica(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
